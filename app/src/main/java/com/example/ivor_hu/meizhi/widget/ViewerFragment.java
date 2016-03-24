df2665482d7ab036039c412c14e39556e530f691
IvorHu <ivorhu@foxmail.com>
2016-03-24 12:19:39 +0800

    修复一个typo；全屏参看图片的临时版本

--- app/src/main/java/com/example/ivor_hu/meizhi/widget/ViewerFragment.java
+++ app/src/main/java/com/example/ivor_hu/meizhi/widget/ViewerFragment.java
@@ -1,5 +1,6 @@
 package com.example.ivor_hu.meizhi.widget;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -28,6 +29,11 @@ public class ViewerFragment extends Fragment implements RequestListener<String, 
     private TouchImageView touchImageView;
     private String mUrl;
     private boolean mInitialShown;
+    private boolean mIsHidden = false;
+
+    private static final int FLAG_IMMERSIVE = View.SYSTEM_UI_FLAG_IMMERSIVE
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,7 +49,9 @@ public class ViewerFragment extends Fragment implements RequestListener<String, 
         touchImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((ViewerActivity) getActivity()).toggleToolbar();
+                ((ViewerActivity) getActivity()).toggleSysbar();
+                toggleToolbar();
+//                toggleHideyBar(touchImageView);
             }
         });
 
@@ -94,6 +102,71 @@ public class ViewerFragment extends Fragment implements RequestListener<String, 
         if (mInitialShown) {
             getActivity().supportStartPostponedEnterTransition();
         }
+    }
+
+    public void toggleToolbar() {
+        int flag = touchImageView.getSystemUiVisibility();
+        if (mIsHidden) {
+//            touchImageView.setSystemUiVisibility(flag & ~FLAG_IMMERSIVE);
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            );
+        } else {
+//            touchImageView.setSystemUiVisibility(flag | FLAG_IMMERSIVE);
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
+        mIsHidden = !mIsHidden;
+    }
+
+    public void toggleHideyBar(View view) {
+
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = view.getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        boolean isImmersiveModeEnabled =
+                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i(TAG, "Turning immersive mode mode off. ");
+        } else {
+            Log.i(TAG, "Turning immersive mode mode on.");
+        }
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY & View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN & View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        }
+
+        view.setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
     }
 
     public View getSharedElement() {
